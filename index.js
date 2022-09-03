f215ee7c5aa829da295350126e3a5de1fff1ee19
Niranjan <niranjanchintuwin2004@gmail.com>
2022-09-03 12:38:07 +0530

    error correction

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 //jshint esversion:6
 const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+var app = express();
+
+app.use(express.static(__dirname +'public'));
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/signUp.html"); 
-});
-
-app.post("/", function (req, res) {
-    const name = req.body.firstName;
-    console.log(req.body.password);
-    req.body.password = Number(req.body.password);
-    console.log(req.body.password);
-    res.send("thanks! "+name);
 });
 
 app.listen(3000, function () {
